f123749209a05c6dc8fda638b27136d7768ca958
Russ Evans <russ.evans@ntiva.com>
2018-12-07 14:55:43 -0500

    Rename Strings Added

--- Mailbox/Office365-RoomLists.ps1
+++ Mailbox/Office365-RoomLists.ps1
@@ -26,6 +26,12 @@ New-DistributionGroup -Name "London" -DisplayName "London" –PrimarySmtpAddress
 #Add Room to Group. In this case London is a room list, and Archimedes1 is the Room being added. 
 Add-DistributionGroupMember –Identity "London" -Member Archimedes1@domain.com
 
+#Rename Room Mailbox
+Set-User ChiLoopFranklin@domain.com -FirstName "CHI Loop Franklin Conference Room"
+
+#Change MailBox DisplayName
+Set-Mailbox ChiWarnerBigTrain@domain.com -DisplayName "CHI Warner Training Room"
+
 #Modify Settings 
 # The string below Adds the Users name to the Subject, Deletes the Subject, Leaves any attachments, and Leaves and text in the Body of the Invite. The outcome of this is a Room Caledard that shows who booked the room, but doesnt show the subject. 
 Set-CalendarProcessing -Identity NYC-Conf-A@domain.com -AddOrganizerToSubject $true -DeleteSubject $true -DeleteAttachments $false -DeleteComments $false
