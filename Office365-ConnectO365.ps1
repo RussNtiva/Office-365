c2047e10869e42b1d98ed5d2f07bb8ca74806ff2
Russ Evans <russ.evans@ntiva.com>
2018-12-07 14:29:24 -0500

    Add Set Calendar Processing Settings

---
+++ Office365-ConnectO365.ps1
@@ -0,0 +1,18 @@
+﻿#Russell Evans
+#2018
+
+#Conenct to Office 365, and Other Services. 
+
+$credential = Get-Credential
+Import-Module MsOnline
+Connect-MsolService -Credential $credential
+Import-Module SkypeOnlineConnector
+$sfboSession = New-CsOnlineSession -Credential $credential
+Import-PSSession $sfboSession
+$exchangeSession = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri "https://outlook.office365.com/powershell-liveid/" -Credential $credential -Authentication "Basic" -AllowRedirection
+Import-PSSession $exchangeSession -DisableNameChecking
+$ccSession = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://ps.compliance.protection.outlook.com/powershell-liveid/ -Credential $credential -Authentication Basic -AllowRedirection
+Import-PSSession $ccSession -Prefix cc 
+Connect-SPOService -Url https://appriver3651007591-admin.sharepoint.com
+
+
--- Office365-RoomLists.ps1
+++ Office365-RoomLists.ps1
@@ -4,13 +4,13 @@
 #Get Commands : 
 
 #Get Basic Info about a Room MB
-Get-Mailbox -Identity "Room Name" 
+Get-Mailbox -Identity "Test Room" | Format-List 
 
 #Gets listing of Rooms setup with the Room Finder
 Get-DistributionGroupMember -Identity "Room Finder" 
 
 #Get listing of All RoomMailboxes
-Get-Mailbox -ResultSize unlimited | select name,RecipientTypeDetails | Where {$_.RecipientTypeDetails-eq "RoomMailbox"}
+Get-Mailbox -ResultSize unlimited | select name,PrimarySmtpAddress,RecipientTypeDetails | Where {$_.RecipientTypeDetails-eq "RoomMailbox"}
 
 #Get Any room lists that have been setup : 
 Get-DistributionGroup | select Displayname,RecipientTypeDetails | Where {$_.RecipientTypeDetails-eq "RoomList"}
@@ -22,3 +22,7 @@ New-DistributionGroup -Name "London" -DisplayName "London" –PrimarySmtpAddress
 
 #Add Room to Group. In this case London is a room list, and Archimedes1 is the Room being added. 
 Add-DistributionGroupMember –Identity "London" -Member Archimedes1@domain.com
+
+#Modify Settings 
+
+Set-CalendarProcessing -Identity NYC-Conf-A@domain.com -AddOrganizerToSubject $true -DeleteSubject $true
