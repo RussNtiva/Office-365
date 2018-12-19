b91d1c4f59be82d85309cc8ff3663b747444855f
Russ Evans <russ.evans@ntiva.com>
2018-12-19 15:22:00 -0500

    Get Distro Listing

--- Distribution List/Create O365 Distribution Lists.ps1
+++ Distribution List/Create O365 Distribution Lists.ps1
@@ -1,6 +1,8 @@
 #Create Distribution Lists in O365 
-New-DistributionGroup -Name "ACG_Region_US" -Type Distribution -PrimarySmtpAddress "ACG_Region_US@ankura.com"
+New-DistributionGroup -Name "ACG_Region_US" -Type Distribution -PrimarySmtpAddress "ACG_Region_US@domain.com"
 #Change Primary SMTP Address of Group
-Set-DistributionGroup -Identity "ACG_Region_US" -PrimarySmtpAddress "ACG_Region_US@ankura.com"
+Set-DistributionGroup -Identity "ACG_Region_US" -PrimarySmtpAddress "ACG_Region_US@domain.com"
 #Import Users from CSV
 Import-Csv 'C:\ActiveScripts\Distribution Groups\ACG_Region_US.csv' | foreach{Add-DistributionGroupMember -Identity "ACG_Region_US" -Member $_.UPN}
+
+Get-DistributionGroupMember -Identity "ACG_Region_US" -ResultSize unlimited | Export-Csv -Path 'C:\ActiveScripts\Distribution Groups\ACG_Region_US_listing.csv'
