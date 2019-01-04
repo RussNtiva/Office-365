da315760b75e3827827c7952d83971a14ce822d1
Russ Evans <russ.evans@ntiva.com>
2019-01-04 11:36:50 -0500

    CS Policy

---
+++ Skype for Business/Custom Client Policy.ps1
@@ -0,0 +1,12 @@
+﻿#To get a list of all the policies and the corresponding IM settings
+Get-CsClientPolicy | ft Identity,DisableSavingIM,EnableIMAutoArchiving
+#To get only the policies with disabled conversation historya
+Get-CsClientPolicy | ? {$_.DisableSavingIM -eq $true} | ft Identity,DisableSavingIM,EnableIMAutoArchiving
+#To See what policy is Enabled for a user.
+Get-CsOnlineUser -Identity user@domain.com | ft ClientPolicy
+#Get Policy Information
+Get-CsClientPolicy -Identity ClientPolicyNoSaveIMNoArchivingNoIMURL
+#Set CS Policy for a particular User.
+Grant-CsClientPolicy -PolicyName Tag:ClientPolicyNoSaveIMNoArchivingNoIMURL -Identity user@domain.com
+Grant-CsClientPolicy -PolicyName Tag:ClientPolicyNoSaveIMNoArchivingNoIMURL -Identity user@domain.com
+
